765fd8fad000d9ddc23190d75eee5e71ae030fb5
FeliciaFebbyutari <123058834+AmelChan-MD@users.noreply.github.com>
2026-03-05 20:11:44 +0700

    Update config.js
    Signed-off-by: FeliciaFebbyutari  <123058834+AmelChan-MD@users.noreply.github.com>

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const moment = require("moment-timezone");
 
 const config = {
-    phone_number_bot: '628988219165',
+    phone_number_bot: '6282234856655',
     owner_number: '6281227978977',
     name_bot: 'XenoviaBot',
     name_owner: 'Xenovia',
